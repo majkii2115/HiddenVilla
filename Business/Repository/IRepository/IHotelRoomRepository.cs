da14578682705622de528f1af13e365ac471ce75
majkii2115 <szymbaramichal@gmail.com>
2021-01-04 16:30:00 +0100

    Adding DB and other projects

---
+++ Business/Repository/IRepository/IHotelRoomRepository.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Repository.IRepository
+{
+    interface IHotelRoomRepository
+    {
+        Task<HotelRoomDTO> CreateHotelRoom(HotelRoomDTO hotelRoomDTO);
+        Task<HotelRoomDTO> UpdateHotelRoom(int roomId, HotelRoomDTO hotelRoomDTO);
+        Task<HotelRoomDTO> GetHotelRoom(int roomId);
+        Task<int> DeleteHotelRoom(int roomId);
+        Task<IEnumerable<HotelRoomDTO>> GetAllHotelRooms();
+        Task<HotelRoomDTO> IsRoomUnique(string name);
+    }
+}
--- HiddenVilla_Server/Startup.cs
+++ HiddenVilla_Server/Startup.cs
@@ -1,8 +1,11 @@
+using AutoMapper;
+using DataAccess.Data;
 using HiddenVilla_Server.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +29,8 @@ namespace HiddenVilla_Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
